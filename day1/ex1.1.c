a583d56d4426df3a948ab38716e495919c94a6f8
JordiDevv <jordidevv@gmail.com>
2024-12-04 16:12:57 +0100

    Se ha modificado la estructura de carpetas, ya que, al realizar la segunda parte del primer día, nos hemos dado cuenta que era una complementación de la algortimia de la primera; así que lo hemos reducido a un único script con toda la lógica para ambos problemas. La nueva implementación es básicamente una función que cuenta la cantidad de veces que se repite un número de la lista de la izquierda en la lista de la derecha, y luego multiplica ese número por esa cantidad de veces. Se ha añadido un pequeño flujo condicional donde ahora se espera ÚNICAMENTE un '1', o un '2', como argumento del programa, para que te escriba la solución a la primera parte, o a la segunda, respectivamente; por supuesto, el programa está protegido para que no se le pueda poner otra cosa. Se han cambiado todas las funciones del script por estáticas; y ahora se almacenan los valores a imprimir en dos variables, para poder liberar las listas antes de finalizar el programa. Se han añadido saltos de línea a los mensajes de error, y se ha quitado la definición de la estructura comentada, pensada para el prototipo anterior.

---
+++ day1/ex1.1.c
@@ -0,0 +1,135 @@
+#include "ex1.1.h"
+
+static int similarity_score(int *left_list, int *right_list)
+{
+	int i = 0;
+	int total = 0;
+
+	while (i < 1000)
+	{
+		int j = 0;
+		int similarity = 0;
+		while(j < 1000)
+		{
+			if (left_list[i] == right_list[j])
+				similarity++;
+			j++;
+		}
+		total += left_list[i] * similarity;
+		i++;
+	}
+
+	return (total);
+}
+
+static int comp_lists(int *left_list, int *right_list)
+{
+	int i = 0;
+	int total = 0;
+
+	while (i < 1000)
+	{
+		int diff = left_list[i] - right_list[i];
+		if (diff < 0)
+			diff *= -1;
+		total += diff;
+		i++;
+	}
+
+	return (total);
+}
+
+static void sort_list(int *list)
+{
+	int	i = 1;
+	int	aux = 0;
+
+	while (i < 1000)
+	{
+		int j = i;
+		while (list[j] < list[j - 1])
+		{
+			aux = list[j];
+			list[j] = list[j - 1];
+			list[j - 1] = aux;
+			if (j > 1)
+				j--;
+			else
+				break ;
+		}
+		i++;
+	}
+}
+
+static void fill_lists(int *left_list, int *right_list, int lst_fd)
+{
+	int		i = 0;
+	char	*line = NULL;
+	char	**split_line = NULL;
+
+	while (i < 1000)
+	{
+		line = get_next_line(lst_fd);
+		if (!line)
+		{
+			printf (R "Error getting one line\n" RE);
+			exit (EXIT_FAILURE);
+		}
+		split_line = ft_split(line, ' ');
+		if (!split_line)
+		{
+			printf (R "Error spliting one line\n" RE);
+			if (line)
+				free (line);
+			exit (EXIT_FAILURE);
+		}
+		free (line);
+		left_list[i] = atoi(split_line[0]);
+		right_list[i] = atoi(split_line[1]);
+		free (split_line[0]);
+		free (split_line[1]);
+		free (split_line);
+		i++;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 2 || (argv[1][0] != '1' && argv[1][0] != '2') || ft_strlen(argv[1]) != 1)
+	{
+		printf(R "Please, type 1 or 2 as argument for the program\n" RE);
+		return (0);
+	}
+    int	*left_list = malloc(1000 * sizeof(int) + 1);
+	if (!left_list)
+	{
+		printf(R "Error allocating memory for the left list\n" RE);
+		return (1);
+	}
+	int	*right_list = malloc(1000 * sizeof(int) + 1);
+	if (!right_list)
+	{
+		printf(R "Error allocating memory for the right list\n" RE);
+		return (1);
+	}
+    int		lst_fd = open("input_list.txt", O_RDONLY);
+    if (lst_fd < 0)
+    {
+        printf(R "Error: Couldn't open the list file\n" RE);
+        return (1);
+    }
+
+    fill_lists(left_list, right_list, lst_fd);
+	sort_list(left_list);
+	sort_list(right_list);
+
+	int diff = comp_lists(left_list, right_list);
+	int similarity = similarity_score(left_list, right_list);
+
+	if (argv[1][0] == '1')
+		printf("%i\n", diff);
+	else if (argv[1][0] == '2')
+		printf("%i\n", similarity);
+
+	return (0);
+}
---
+++ day1/get_next_line/get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsanz-bo <jsanz-bo@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/21 17:44:30 by jsanz-bo          #+#    #+#             */
+/*   Updated: 2024/06/21 17:44:30 by jsanz-bo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include "../../jslib.h"
+
+char	*get_next_line(int fd);
+char	*ft_free(char **str);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+
+#endif
