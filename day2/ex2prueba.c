86e39e0ad053fd2787ca3f6820848d39dd35c395
JordiDevv <jordidevv@gmail.com>
2024-12-05 09:31:11 +0100

    Se ha creado un algoritmo distinto para gestionar los casos del problem_dampener, pero sin éxito. Los guardo para trabajar en ambas alternativas, a ver si con alguna llegamos al resultado correcto.

---
+++ day2/ex2prueba.c
@@ -0,0 +1,134 @@
+#include "../jslib.h"
+
+static void get_next_line_splited(int input_fd, char ***split_line)
+{
+    char    *line = NULL;
+
+    line = get_next_line(input_fd);
+    if (!line)
+    {
+        printf(R "Error getting next line" RE);
+        exit(EXIT_FAILURE);
+    }
+    *split_line = ft_split(line, ' ');
+    free(line);
+    if (!*split_line)
+    {
+        printf(R "Error spliting one line" RE);
+        exit(EXIT_FAILURE);
+    }
+}
+
+static int cast_line(int **report, char **split_line)
+{
+    int i = 0;
+    int len = 0;
+
+    for (int i = 0; split_line[i]; i++)
+        len++;
+    *report = malloc(len * 4);
+    for (int i = 0; split_line[i]; i++)
+        (*report)[i] = atoi(split_line[i]);
+
+    return (len);
+}
+
+static int check_aux(int *report, int len)
+{
+    int i = 0;
+    int asc_flag = 0;
+    if ((report[i + 1] - report[i] > 0 && report[i + 2] - report[i + 1] > 0)
+            || (report[i + 1] - report[i] > 0 && report[i + 3] - report[i + 2] > 0))
+        asc_flag = 1;
+
+    while (i < len - 1)
+    {
+        int diff = report[i + 1] - report[i];
+        if (!((diff > 0 && diff < 4 && asc_flag) || (diff < 0 && diff > -4 && !asc_flag)))
+            return (0);
+        i++;
+    }
+
+    return (1);
+}
+
+static int check_report(int *report, int len, int problem_dampener)
+{
+    int i = 0;
+    int asc_flag = 0;
+    if ((report[i + 1] - report[i] > 0 && report[i + 2] - report[i + 1] > 0)
+            || (report[i + 1] - report[i] > 0 && report[i + 3] - report[i + 2] > 0))
+        asc_flag = 1;
+
+    while (i < len - 1)
+    {
+        if (i >= len - 2 && problem_dampener)
+            return (1);
+        int diff = report[i + 1] - report[i];
+        if (!((diff > 0 && diff < 4 && asc_flag) || (diff < 0 && diff > -4 && !asc_flag)))
+        {
+            if (problem_dampener)
+            {
+                int *aux_report1 = malloc((len - 1) * 4);
+                int *aux_report2 = malloc((len - 1) * 4);
+                int j = 0, k = 0;
+                while (j < len - 1)
+                {
+                    if (k == i)
+                        k++;
+                    aux_report1[j] = report[k];
+                    j++;
+                    k++;
+                }
+                j = 0, k = 0;
+                while (j < len - 1)
+                {
+                    if (k == i + 1)
+                        k++;
+                    aux_report2[j] = report[k];
+                    j++;
+                    k++;
+                }
+                int value = check_aux(aux_report1, len - 1) + check_aux(aux_report2, len - 1);
+                value = value > 1 ? 1 : value;
+                problem_dampener--;
+                free(aux_report1);
+                free(aux_report2);
+                return (value);
+            }
+            else
+                return (0);
+        }
+        i++;
+    }
+
+    return (1);
+}
+
+int main()
+{
+    int		input_fd = open("input_list.txt", O_RDONLY);
+    if (input_fd < 0)
+    {
+        printf(R "Error: Couldn't open the list file\n" RE);
+        return (1);
+    }
+
+    char    **split_line = NULL;
+    int     *report;
+    int     i = 0;
+    int     safe_reports = 0;
+
+    while (i < 1000)
+    {
+        get_next_line_splited(input_fd, &split_line);
+        int len = cast_line(&report, split_line);
+        i++;
+        free_mat(split_line, len);
+        safe_reports += check_report(report, len, 1);
+        free(report);
+    }
+
+    printf(G "%i\n" RE, safe_reports);
+    return (0);
+}
