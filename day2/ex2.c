ef9b0de8db5428661da13ee8c4a7ca9ef874b161
JordiDevv <jordidevv@gmail.com>
2024-12-05 03:21:37 +0100

    La segunda parte requiere de una pequeña implementación que consiste en un problem_dampener para poder eliminar un nivel, en caso de que, de esa manera, el reporte sea seguro. Hemos agregado un flag en check_report y, lo que a priori parecía bastante sencillo, nos está costando bastante de solucionar. Tenemos una lógica bastante consistente, pienso yo, pero nos estamos volviendo locos pensando diferentes posibilidades y contemplándolas con diferentes condiciones, de momento lo vamos a dejar por aquí, es muy tarde.

--- day2/ex2.c
+++ day2/ex2.c
@@ -37,14 +37,48 @@ static int check_report(int *report, int len)
 {
     int i = 0;
     int asc_flag = 0;
-    if (report[i + 1] - report[i] > 0)
+    int problem_dampener = 1;
+    if ((report[i + 1] - report[i] > 0 && report[i + 2] - report[i + 1] > 0)
+            || (report[i + 1] - report[i] > 0 && report[i + 3] - report[i + 2] > 0))
         asc_flag = 1;
+    
+    int diff = report[i + 1] - report[i];
+    if (!((diff > 0 && diff < 4 && asc_flag) || (diff < 0 && diff > -4 && !asc_flag))
+            || diff == 0)
+    {
+        i++;
+        problem_dampener--;
+    }
 
     while (i < len - 1)
     {
-        int diff = report[i + 1] - report[i];
+        if (i >= len - 2 && problem_dampener)
+            return (1);
+        diff = report[i + 1] - report[i];
         if (!((diff > 0 && diff < 4 && asc_flag) || (diff < 0 && diff > -4 && !asc_flag)))
-            return (0);
+        {
+            if (problem_dampener)
+            {
+                diff = report[i + 2] - report[i];
+                if ((diff > 0 && diff < 4 && asc_flag) || (diff < 0 && diff > -4 && !asc_flag))
+                {
+                    i += 2;
+                    problem_dampener--;
+                    continue ;
+                }
+                diff = report[i + 1] - report[i - 1];
+                if ((diff > 0 && diff < 4 && asc_flag) || (diff < 0 && diff > -4 && !asc_flag))
+                {
+                    i += 2;
+                    problem_dampener--;
+                    continue ;
+                }
+                else
+                    return (0);
+            }
+            else
+                return (0);
+        }
         i++;
     }
 
