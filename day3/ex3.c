1610a82cc05ffb30ec14a989690af7fe32cfb1e2
JordiDevv <jordidevv@gmail.com>
2024-12-07 19:41:09 +0100

    Se ha ajustado el input para hacer algunas pruebas: al menos con lo eso funciona correctamente. Se ha hecho lo que se propuso; el concepto es sacar substr de cada bloque válido de la línea y extraer el resultado de la operación, mientras vamos iterando y sumándola. Hay que terminar de probarlo y estaría bien refactorizar un poco para dejarlo algo más limpio, pero parece un prototipo funcional y válido.

--- day3/ex3.c
+++ day3/ex3.c
@@ -1,58 +1,82 @@
 #include "../jslib.h"
 
-int	ft_isdigit(int c)
+static int	ft_isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 		return (1);
 	return (0);
 }
 
+static int get_valid_mul(char *line)
+{
+    char    *beg = line;
+    int     result_line = 0;
+
+    while (*beg)
+    {
+        int *operands = malloc(2 * sizeof(int));
+        beg = ft_strchr(beg, 'm');
+        if (!beg)
+        {
+            free(operands);
+            return (result_line);
+        }
+        if (!ft_strncmp(beg, "mul(", 4))
+        {
+            char *end = beg + 4;
+            beg = end;
+            for (int i = 0; i < 3; i++)
+            {
+                if (!ft_isdigit(*end))
+                    break ;
+                end++;
+            }
+            if (*end != ',')
+                continue ;
+            char *n = ft_substr(line, beg - line, end - beg);
+            operands[0] = atoi(n);
+            free(n);
+            end++;
+            beg = end;
+            for (int i = 0; i < 3; i++)
+            {
+                if (!ft_isdigit(*end))
+                    continue ;
+                end++;
+            }
+            if (*end != ')')
+                continue ;
+            n = ft_substr(line, beg - line, end - beg);
+            operands[1] = atoi(n);
+            free(n);
+            result_line += operands[0] * operands[1];
+            free(operands);
+        }
+        beg++;
+    }
+    return (result_line);
+}
 
 int main()
 {
-    int fd_input = open("input_data", O_RDONLY);
+    int fd_input = open("input_data.txt", O_RDONLY);
     if (fd_input < 0)
     {
-        printf(R "Error opening the data file" RE);
+        printf(R "Error opening the data file\n" RE);
         return (1);
     }
 
     char    *line = get_next_line(fd_input);
-    char    **mul_mat = malloc(1);
     int     i = 0;
-    int     total_len = 0;
+    int     result = 0;
+
     while (line)
     {
-        while (line)
-        {
-            char *beg = ft_strchr(line, 'm');
-            if (!ft_strncmp(beg, "mul(", 4))
-            {
-                char *end = beg + 5;
-                for (int i = 0; i > 3; i++)
-                {
-                    if (!ft_isdigit(*end))
-                        continue ;
-                    end++;
-                }
-                if (*end != ',')
-                    continue ;
-                end++;
-                for (int i = 0; i > 3; i++)
-                {
-                    if (!ft_isdigit(*end))
-                        continue ;
-                    end++;
-                }
-                if (*end != ')')
-                    continue ;
-                char *valid_mul = ft_substr(line, beg, end - beg + 1);
-                total_len += ft_strlen(valid_mul);
-                realloc(mul_mat, total_len);
-                mul_mat[i] = malloc(ft_strlen(valid_mul));
-            }
-        }
+        result += get_valid_mul(line);
         free(line);
         line = get_next_line(fd_input);
     }
+
+    printf(G "%i" RE, result);
+    return (0);
 }
